f9c4355d09f7e743a514cc7c12dfadebb4276a97
Cristhian Castro <ccastrove.05@gmail.com>
2026-02-22 18:23:57 -0500

    refactor: convert gold dataset creation into function for pipeline orchestration

--- src/gold_dataset.py
+++ src/gold_dataset.py
@@ -2,34 +2,37 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-SILVER_PATH = "data/silver/cobranza_clean.parquet"
-GOLD_PATH = "data/gold/"
 
-os.makedirs(GOLD_PATH, exist_ok=True)
+def create_gold_dataset():
 
-print("Cargando datos Silver...")
-df = pd.read_parquet(SILVER_PATH)
+    SILVER_PATH = "data/silver/cobranza_clean.parquet"
+    GOLD_PATH = "data/gold/"
 
-TARGET = "pago_30d"
+    os.makedirs(GOLD_PATH, exist_ok=True)
 
-X = df.drop(columns=[TARGET])
-y = df[TARGET]
+    print("Cargando datos Silver...")
+    df = pd.read_parquet(SILVER_PATH)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42,
-    stratify=y
-)
+    TARGET = "pago_30d"
 
-train_df = X_train.copy()
-train_df[TARGET] = y_train.values
+    X = df.drop(columns=[TARGET])
+    y = df[TARGET]
 
-test_df = X_test.copy()
-test_df[TARGET] = y_test.values
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
 
-train_df.to_parquet(os.path.join(GOLD_PATH, "train.parquet"), index=False)
-test_df.to_parquet(os.path.join(GOLD_PATH, "test.parquet"), index=False)
+    train_df = X_train.copy()
+    train_df[TARGET] = y_train.values
 
-print("✔ Gold generado correctamente.")
+    test_df = X_test.copy()
+    test_df[TARGET] = y_test.values
+
+    train_df.to_parquet(os.path.join(GOLD_PATH, "train.parquet"), index=False)
+    test_df.to_parquet(os.path.join(GOLD_PATH, "test.parquet"), index=False)
+
+    print("✔ Gold generado correctamente.")
